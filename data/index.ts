23869a4cf3531cf75f84d829ff350e8f96114ff5
GoatsOnTheMoon <121653725+GoatsOnTheMoon@users.noreply.github.com>
2024-05-18 17:34:11 -0400

    Update index.ts
    1

--- data/index.ts
+++ data/index.ts
@@ -173,29 +173,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Fletcher Moon",
+    desc: "Lead Front End Developer of our web and mobile based apps, utilizing react.js, next.js, and react native.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Kara Lackey",
+    desc: "Leads our enterprise sales team for both education and enterprise based customers",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Austin Karisny",
+    desc: "Founder of Good Gains, leading innovation and customer acquisition. Proficient in full stack development and management.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Austin Lott",
+    desc: " Lead Back End Developer of our profile system and badge api, utilizing next.js, ether.js and typescript",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
