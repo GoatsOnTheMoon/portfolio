cdae99de5bb3e0b2a9932123c5983f33b7c4f7c5
goodgains-dev <159225098+goodgains-dev@users.noreply.github.com>
2024-05-18 17:42:14 -0400

    Update RecentProjects.tsx
    1

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -20,7 +20,7 @@ const RecentProjects = () => {
           >
             <PinContainer
               title="https://goodgainsgram.vercel.app/"
-              href="https://twitter.com/mannupaaji"
+              href="https://goodgainsgram.vercel.app/"
             >
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
